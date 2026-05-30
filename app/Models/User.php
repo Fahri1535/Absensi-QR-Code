b7b14a02e0baa86daa9b4ecf8afdf54a8510a66b
Mohamad Fahri Akbar <mfahriakbar15@gmail.com>
2026-05-30 23:08:57 +0700

    Fix missing model imports in User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Karyawan;
+use App\Models\Notifikasi;
 
 class User extends Authenticatable
 {
